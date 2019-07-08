70081f1999f81193b42e95e985ea5f64f653471d
Brad Bartlett <bradbartlett1@me.com>
2019-07-08 14:16:39 -0400

    controllers & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+patrick, squidward, spongebob, gary, krabs
+krusty_krab - american, chum bucket - italian, goofy_goobers - ice cream
+
+lisa, marge, homer, bart, maggie
+krusty_burger - american, kwik-e-mart - ice cream, moes - bar
+
+stan, kyle, kenny, kartman, kennys_mom
+bennys - mexican, chefs_cafateria - american, city_wok - chinese
+
+tina, bob, louise, linda, gene
+bobs_burgers - american, falafel_waffle - breakfast, jimmy_pestos - italian
