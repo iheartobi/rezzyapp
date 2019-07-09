8869c9cb216320004f87a4253b83088032fdb9f3
iheartobi <iobidah9@gmail.com>
2019-07-09 11:14:34 -0400

    relationship updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
     has_many :reservations
     has_many :restaurants, through: :reservations
 
+    has_many :followers, through: :follower_follows, source: :follower
+    has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
+
+    has_many :followees, through: :followee_follows, source: :followee 
+    has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
+
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
