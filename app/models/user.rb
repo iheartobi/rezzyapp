9caf63774f824a6b48e9ba49bcab263652e25bd9
Brad Bartlett <bradbartlett1@me.com>
2019-07-09 11:32:51 -0400

    message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
     has_many :reservations
     has_many :restaurants, through: :reservations
 
-    # has_secure_password
-
-    # validates :username, presence: true, uniqueness: true
+    
     
 end
