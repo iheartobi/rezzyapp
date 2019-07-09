c95b60d035fc92c5efb8f80c12b844192f1395a6
Brad Bartlett <bradbartlett1@me.com>
2019-07-09 14:18:43 -0400

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to 'sessions#destroy'
 resources :restaurants
 resources :menu_items
 resources :users
 resources :reservations
 resources :followee
+
 
 end
