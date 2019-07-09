2d7f509f9f6905771952ab8660def9ea56c6cc9e
Brad Bartlett <bradbartlett1@me.com>
2019-07-09 11:39:37 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :restaurants
-resources :reviews
+resources :menu_items
 resources :users
 resources :reservations
+resources :followee
 
 end
