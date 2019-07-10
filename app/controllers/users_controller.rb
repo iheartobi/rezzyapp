c6530cfccf8767f819c0923ae360898fdcb8f7ff
iheartobi <iobidah9@gmail.com>
2019-07-10 12:02:47 -0400

    friendships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    # before_action :authenticate!, except: [:index, :show]
+    #  before_action :authenticate!, except: [:index, :show]
 
     def index 
         @users = User.all
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user = User.new
+        @user = User.new(user_params)
         if @user.save
-            redirect_to users_path
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
             render :new
         end
